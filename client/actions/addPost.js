b2560e9f2cd6cabf56452079e9cc6a4e8cc97671
Hamish Mourie <20253849+hamish-mourie@users.noreply.github.com>
2022-09-23 11:33:28 +1200

    Add addPost actions and reducer

---
+++ client/actions/addPost.js
@@ -0,0 +1,29 @@
+export const ADD_POST_REQUEST = 'ADD_POST_REQUEST'
+export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS'
+export const ADD_POST_FAILURE = 'ADD_POST_FAILURE'
+import { addPost } from '../apis/posts'
+
+export const addPostRequest = () => ({
+  type: ADD_POST_REQUEST,
+})
+
+export const addPostSuccess = (post) => ({
+  type: ADD_POST_SUCCESS,
+  payload: { post },
+})
+
+export const addPostFailure = (errMessage) => ({
+  type: ADD_POST_FAILURE,
+  payload: { errMessage },
+})
+
+export const addNewPost = (post) => (dispatch) => {
+  dispatch(addPostRequest())
+  return addPost(post)
+    .then((post) => {
+      dispatch(addPostSuccess(post))
+    })
+    .catch((error) => {
+      dispatch(addPostFailure(error.message))
+    })
+}
---
+++ client/reducers/addPost.js
@@ -0,0 +1,25 @@
+import {
+  ADD_POST_FAILURE,
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+} from '../actions/addPost'
+
+const initialState = {
+  data: null,
+  loading: true,
+  error: null,
+}
+export default function myPostReducer(state = initialState, action) {
+  const { type, payload } = action
+
+  switch (type) {
+    case ADD_POST_REQUEST:
+      return { ...state, loading: true, error: null }
+    case ADD_POST_SUCCESS:
+      return { data: payload.post, loading: false, error: null }
+    case ADD_POST_FAILURE:
+      return { ...state, loading: false, error: payload.error }
+    default:
+      return state
+  }
+}
