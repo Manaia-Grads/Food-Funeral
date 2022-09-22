39f0ded90eca153acab65563e2fae413831ea5b6
ling_zhang <46408000+ling-starts-code@users.noreply.github.com>
2022-09-22 16:56:59 +1200

    started CSS with tailwind

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,11 +4,14 @@ import Posts from './Posts'
 const App = () => {
   return (
     <div>
-      <h1>Food Funeral</h1>
-      <h3>Food Blogging with a conscience.</h3>
-
-      <Posts />
+      <header className={'pt-2 pb-5'}>
+        <h1 className={"text-6xl text-center"}>Food Funeral</h1>
+        <h3 className={'text-3xl text-center'}>Food Blogging with a conscience.</h3>
+      </header>
+      <main>
+        <Posts />
       {/* getPostById(2).then((reply) => {console.log(reply)}) */}
+      </main>
     </div>
   )
 }
--- client/components/Posts.jsx
+++ client/components/Posts.jsx
@@ -7,24 +7,22 @@ function Posts() {
   useEffect(() => dispatch(fetchPosts()),[])
 
   const {data, loading, error } = useSelector(state => state.posts)
-  console.log('component', data, 'loading', loading)
 
   return (
     <>    
     <div>
-      {!error && loading? (<div> loading...</div>) :(  
-      <>
-      {data?.map(post => (
-        <div key={post.id}> 
-          <h3>{post.title}</h3>
-          <img src={post.img} alt='unfortunately there is no alt text available right now'/>
-          <p>By TBC - username</p>
-        </div>
-        )
-      )}
-     </>
+      {!error && loading? (<div className={"text-l text-center"}><img className={'place-self-center'}src={'/images/loading.gif'} alt='different food type spinning'/> </div>) :(  
+      <div className={'flex'}>
+        {data?.map(post => (
+          <div key={post.id} className={'flex-auto'}> 
+            <h3 className={'text-3xl font-mono'}>{post.title}</h3>
+            <img className={'w-3/4'} src={'/images/'+post.img} alt='unfortunately there is no alt text available right now'/>
+            <p className={'text-blue-400'}>By TBC - username</p>
+          </div>
+          )
+        )}
+     </div>
      )}
-    
     </div>
     </>
   )
