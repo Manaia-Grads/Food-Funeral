11b6bd95f3d945e5bcc6adbf6370ac0cd4514fe9
ling_zhang <46408000+ling-starts-code@users.noreply.github.com>
2022-09-22 16:57:32 +1200

    run prettier

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,12 +5,14 @@ const App = () => {
   return (
     <div>
       <header className={'pt-2 pb-5'}>
-        <h1 className={"text-6xl text-center"}>Food Funeral</h1>
-        <h3 className={'text-3xl text-center'}>Food Blogging with a conscience.</h3>
+        <h1 className={'text-6xl text-center'}>Food Funeral</h1>
+        <h3 className={'text-3xl text-center'}>
+          Food Blogging with a conscience.
+        </h3>
       </header>
       <main>
         <Posts />
-      {/* getPostById(2).then((reply) => {console.log(reply)}) */}
+        {/* getPostById(2).then((reply) => {console.log(reply)}) */}
       </main>
     </div>
   )
--- client/components/Posts.jsx
+++ client/components/Posts.jsx
@@ -1,29 +1,40 @@
-import React, {useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchPosts } from '../actions/posts'
 
 function Posts() {
   const dispatch = useDispatch()
-  useEffect(() => dispatch(fetchPosts()),[])
+  useEffect(() => dispatch(fetchPosts()), [])
 
-  const {data, loading, error } = useSelector(state => state.posts)
+  const { data, loading, error } = useSelector((state) => state.posts)
 
   return (
-    <>    
-    <div>
-      {!error && loading? (<div className={"text-l text-center"}><img className={'place-self-center'}src={'/images/loading.gif'} alt='different food type spinning'/> </div>) :(  
-      <div className={'flex'}>
-        {data?.map(post => (
-          <div key={post.id} className={'flex-auto'}> 
-            <h3 className={'text-3xl font-mono'}>{post.title}</h3>
-            <img className={'w-3/4'} src={'/images/'+post.img} alt='unfortunately there is no alt text available right now'/>
-            <p className={'text-blue-400'}>By TBC - username</p>
+    <>
+      <div>
+        {!error && loading ? (
+          <div className={'text-l text-center'}>
+            <img
+              className={'place-self-center'}
+              src={'/images/loading.gif'}
+              alt="different food type spinning"
+            />{' '}
           </div>
-          )
+        ) : (
+          <div className={'flex'}>
+            {data?.map((post) => (
+              <div key={post.id} className={'flex-auto'}>
+                <h3 className={'text-3xl font-mono'}>{post.title}</h3>
+                <img
+                  className={'w-3/4'}
+                  src={'/images/' + post.img}
+                  alt="unfortunately there is no alt text available right now"
+                />
+                <p className={'text-blue-400'}>By TBC - username</p>
+              </div>
+            ))}
+          </div>
         )}
-     </div>
-     )}
-    </div>
+      </div>
     </>
   )
 }
