e94fb2d955cdfa3cfc002ceee0569216a13bb4dc
Hamish Mourie <20253849+hamish-mourie@users.noreply.github.com>
2022-09-27 10:58:13 +1300

    Add update button & conditional rendering to post page

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { getPost } from '../actions/post.js'
+import { UpdatePost } from './UpdatePost.jsx'
 
 export default function Post() {
   const dispatch = useDispatch()
   const { data: post, loading, error } = useSelector((state) => state.post)
   const { id } = useParams()
+  let renderUpdateButton = false
+
+  const [update, setUpdate] = useState({ updateStatus: false })
 
   useEffect(() => {
     dispatch(getPost(id))
   }, [])
+
+  function clickHandler() {
+    console.log(renderUpdateButton)
+    setUpdate({ updateStatus: true })
+  }
 
   return loading ? (
     'loading'
@@ -32,7 +41,15 @@ export default function Post() {
           <p>Posted By: {post?.name}</p>
         </div>
         <div>{post?.content}</div>
+
+        <button
+          onClick={clickHandler}
+          className="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-green-700 hover:text-white  md:border-2 md:border-purple-900 md:bg-purple-900 md:p-2 md:text-white md:hover:text-white"
+        >
+          Update
+        </button>
       </div>
+      {update.updateStatus && <UpdatePost />}
     </>
   )
 }
---
+++ client/components/UpdatePost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function UpdatePost({ postData }) {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <h1>Update</h1>
+      </div>
+    </>
+  )
+}
