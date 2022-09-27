1bb7b7693baccbf784bcdf2d23ffe2ed0ac5774e
Bleuette <102277517+BleuHere@users.noreply.github.com>
2022-09-27 16:46:48 +1300

    write api tests for getting and adding comments

--- client/apis/__tests__/comments.test.js
+++ client/apis/__tests__/comments.test.js
@@ -1,5 +1,5 @@
 import nock from 'nock'
-import { getCommentsByPostId, getCommentById, addComment } from '../comments'
+import { getCommentsByPostId, addComment } from '../comments'
 
 jest.spyOn(console, 'error')
 
@@ -28,6 +28,8 @@ const fakeComments = [
   },
 ]
 
+const errMessage = 'uh oh, this is an error'
+
 describe('GET /api/v1/posts/:id/comments', () => {
   it('gets an array of comment objects', async () => {
     expect.assertions(2)
@@ -39,11 +41,52 @@ describe('GET /api/v1/posts/:id/comments', () => {
     expect(comments).toHaveLength(2)
     expect(scope.isDone()).toBe(true)
   })
-  it.todo('returns an error on getAllCommentsById')
+  it('returns an error on getCommentsById', async () => {
+    expect.assertions(2)
+
+    const scope = nock(rootURL)
+      .get('/api/v1/posts/1/comments')
+      .replyWithError(errMessage)
+    return getCommentsByPostId(1).then(() => {
+      expect(scope.isDone()).toBeTruthy()
+      expect(console.error).toHaveBeenCalledWith(errMessage)
+    })
+  })
 })
 
+const id = 3
+
+const postId = 1
+
+const fakeNewComment = {
+  content: 'test a beret, really??',
+  auth0_id: 'google-oauth2|103547991597142817347',
+  name: 'Foo Bar',
+  date_created: '1664233155375',
+}
+
 describe('POST /api/v1/posts/:id/comments', () => {
-  it.todo('returns one new comment')
-  it.todo('returns an error on addComment')
-  it.todo('returns an error on getCommentById')
+  it('returns one new comment', async () => {
+    expect.assertions(3)
+
+    const scope = nock(rootURL)
+      .post('/api/v1/posts/1/comments')
+      .reply(200, { ...fakeNewComment, postId, id })
+
+    const newComment = await addComment(fakeNewComment, postId)
+    expect(Object.keys(newComment)).toHaveLength(6)
+    expect(newComment.name).toBe('Foo Bar')
+    expect(scope.isDone()).toBe(true)
+  })
+  it('returns an error on addComment', async () => {
+    expect.assertions(2)
+
+    const scope = nock(rootURL)
+      .post('/api/v1/posts/1/comments')
+      .replyWithError(errMessage)
+    return addComment(fakeNewComment, postId).then(() => {
+      expect(scope.isDone()).toBeTruthy()
+      expect(console.error).toHaveBeenCalledWith(errMessage)
+    })
+  })
 })
