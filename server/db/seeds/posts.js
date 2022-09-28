735891de419454e5b14f58faa449866bc9a1c91f
Grant Scott-Hayes <107601640+grant-scott-hayes@users.noreply.github.com>
2022-09-28 18:54:50 +1300

    added seed and fixed tests

--- server/db/seeds/posts.js
+++ server/db/seeds/posts.js
@@ -10,9 +10,9 @@ exports.seed = function (knex) {
           title: 'Sweet Porky',
           date_eaten: '2022-09-22',
           content:
-            'He went by so many names, Porky, Number Five, Hong Kong Sweet and Sour Pork, but we can all agree he was special; particularly on Wednesday when he was part of the 2-4-1 deal at Dragon Garden BYO Restaurant. It was purely by chance that I met Porky -one evening after a very hard day at the office. I felt an immediate connection to his super sweetness and his unashamed salty side - we can all can relate, I’m sure. Although we only knew each other for a tragically short time, Porky bought me great comfort and a sense of fulfilment in my moment of need. When I learned that he was no longer with us I was devastated. I was left with feelings of guilt and shame at not taking more time to get to know him. I was also gripped by hungry inner emptiness mush sooner than I imagined possible. Porky was a long way from his humble beginning on a farm in the Waikato, and we mustn’t forget the impact his time hanging around in the Phillipines had on his character and flavour. You brought such richness to my life. I’ll miss you friend.',
+            'He went by so many names, Porky, Number Five, Hong Kong Sweet and Sour Pork, but we can all agree he was special; particularly on Wednesday when he was part of the 2-4-1 deal at Dragon Garden BYO Restaurant. It was purely by chance that I met Porky -one evening after a very hard day at the office. I felt an immediate connection to his super sweetness and his unashamed salty side - we can all can relate, I’m sure. Although we only knew each other for a tragically short time, Porky bought me great comfort and a sense of fulfilment in my moment of need. When I learned that he was no longer with us I was devastated. I was left with feelings of guilt and shame at not taking more time to get to know him. I was also gripped by a hungry inner emptiness much sooner than I imagined possible. Porky was a long way from his humble beginning on a farm in the Waikato, and we mustn’t forget the impact his time hanging around in the Phillipines had on his character and flavour. You brought such richness to my life. I’ll miss you friend.',
           img: 'porky.png',
-          auth0_id: 'google-oauth2|103547991597142817347',
+          auth0_id: 'google-oauth2|117026446347775944394',
           name: 'G SH',
           date_created: '2022-09-22',
         },
@@ -23,8 +23,8 @@ exports.seed = function (knex) {
           content:
             'Goodbye Spagett. Some thought it was a mistake that we were friends but I found it deeply rewarding. Lets be honest, things got super messy near the end but that was always in the recipe. Life was certainly more saucy with you at the table.',
           img: 'spaghetti.png',
-          auth0_id: 'google-oauth2|103547991597142817347',
-          name: 'Major Major',
+          auth0_id: 'google-oauth2|117026446347775944394',
+          name: 'G SH',
           date_created: '2022-09-23',
         },
         {
@@ -43,7 +43,7 @@ exports.seed = function (knex) {
           title: 'Banana Caramel Cake',
           date_eaten: '2022-09-03',
           content:
-            'Omg. I feel soooo bad but this cake was delicious. Can not believe Cakey is dead now!!!  It is just like they say… the first is the easiest… Now how will I stop? I hope that my family will forgive me. Fortunately I paid in cash so hopefully no evidence? Ahhh!! I don’t even know why I’m posting this… Surely the internet police will find me now? Okay. I have gotta plan my next step… But first - a cafe trip? >:-)',
+            'Omg. I feel soooo bad but this cake was delicious. Can not believe Cakey is dead now!!! It is just like they say… the first is the easiest… Now how will I stop?                    I hope that my family will forgive me. Fortunately I paid in cash so hopefully no evidence? Ahhh!! I don’t even know why I’m posting this… Surely the internet police will find me now? Okay. I have gotta plan my next step… But first - a cafe trip? >:-)',
           img: 'banana-cake.png',
           auth0_id: 'google-oauth2|103547991597142817347',
           name: 'BleuLovesBrunch',
